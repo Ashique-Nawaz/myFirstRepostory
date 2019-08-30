e41502a2528f615857f3c45f02473c8fc0867ec1
Ashique-Nawaz <ashique.mca.du.2015@gmail.com>
2019-08-30 23:48:21 +0530

    added sample src drectory with a java file

---
+++ src/testProject/TestClassForGit.java
@@ -0,0 +1,10 @@
+package testProject;
+
+public class TestClassForGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my first git project");
+	}
+
+}
